6d3c10ef12f41c0dd5bc909053d7f1ee2888fcff
MdShohan81 <mdshohan6563@gmail.com>
2022-08-01 12:42:18 +0600

    added menubar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Menubar from './Shared/Menubar/Menubar';
+import {Routes, Route } from 'react-router-dom';
+import Home  from './Pages/Home/Home';
+import Appointment from './Pages/Appointment/Appointment';
+import SignIn from './Pages/SignIn/SignIn';
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Menubar></Menubar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+      </Routes>
     </div>
   );
 }
